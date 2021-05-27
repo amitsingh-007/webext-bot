aa3f58f630450028964f7cb55517a14c555e2467
cw-amitsingh <amit.singh@carwale.com>
2021-05-27 20:00:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ module.exports = (app) => {
   app.onAny(async (context) => {
     context.log.info(context);
   });
+
+  app.on("check_run", async (context) => {
+    context.log.info(context);
+  });
+
+  app.on("check_run.completed", async (context) => {
+    context.log.info(context);
+  });
+
+  app.on("check_run.created", async (context) => {
+    context.log.info(context);
+  });
 };
